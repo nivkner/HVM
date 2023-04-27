a08a8c98007a12e4737d7c5b9520ba860b6f6dee
Niv Kaminer <niv.kaminer@cs.technion.ac.il>
2023-04-27 23:53:53 +0300

    add more parameters to LesserTerm's arbitrary

--- tests/interpreter.rs
+++ tests/interpreter.rs
@@ -8,14 +8,19 @@ const MAX_U60: u64 = !0 >> 4;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Op(Oper);
 
-// specifies the range of different identifiers in an expression
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct IdetifierMax(usize);
+pub struct TermParams {
+    max_identifiers: usize,
+    max_depth: usize,
+}
 
-impl Default for IdetifierMax {
+impl Default for TermParams {
     fn default() -> Self {
         // ensures by default there are variabes in expressions by default
-        Self(10)
+        Self {
+            max_identifiers: 10,
+            max_depth: 6,
+        }
     }
 }
 
@@ -41,23 +46,23 @@ impl Arbitrary for Op {
 }
 
 impl Arbitrary for LesserTerm {
-    type Parameters = IdetifierMax;
+    type Parameters = TermParams;
     type Strategy = BoxedStrategy<Self>;
 
     fn arbitrary_with(params: Self::Parameters) -> Self::Strategy {
         use LesserTerm::*;
-        let ident_strat = (0..(params.0)).prop_map(|name| Identifier{ name });
+        let ident_strat = (0..(params.max_identifiers)).prop_map(|name| Identifier{ name });
         let u60_strat = (0..MAX_U60).prop_map(|numb| U60 {numb});
         // remove the last 4 bits to ensure floats match after conversion
         let f60_strat = proptest::num::f64::NORMAL.prop_map(|x| F60 {numb: f64::from_bits((x.to_bits() >> 4) << 4)});
         prop_oneof![ u60_strat, f60_strat, ident_strat].prop_recursive(
-            4, // No more than 4 branch levels deep
+            params.max_depth as _, // No more than `max_depth` levels deep
             64, // Target around 64 total elements
             16, // Each collection is up to 16 elements long
             move |element| {
                 let boxed_elem = element.prop_map(|x| Box::new(x));
                 prop_oneof![
-                    (0..params.0, boxed_elem.clone()).prop_map(|(name, body)| Lam { name, body }),
+                    (0..params.max_identifiers, boxed_elem.clone()).prop_map(|(name, body)| Lam { name, body }),
                     (boxed_elem.clone(), boxed_elem.clone()).prop_map(|(func, argm)| App { func, argm }),
                     (proptest::arbitrary::any::<Op>(), boxed_elem.clone(), boxed_elem.clone()).prop_map(|(oper, val0, val1)| Op2 { oper, val0, val1 }),
                 ]

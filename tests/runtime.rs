9ceebeae91255e669e603f444a886c26088a6d56
Niv Kaminer <niv.kaminer@cs.technion.ac.il>
2023-04-29 19:34:50 +0300

    implement the actual test comparing the model to the real thing (part 1)

--- tests/runtime.rs
+++ tests/runtime.rs
@@ -56,8 +56,32 @@ fn compare_to_model() {
     let params = arbitrary::TermParams::default();
     let runtime = hvm::RuntimeBuilder::default().set_thread_count(1).build();
     proptest!(|(lesser_term in arbitrary::LesserTerm::arbitrary_with(params))| {
-        let term = lesser_term.into();
-        let _result = std::panic::catch_unwind(|| runtime.normalize_term(&term));
+        let term: Term = lesser_term.into();
+        let comp_result = std::panic::catch_unwind(|| {
+            let runtime = &runtime; // shadow to prevent moving
+            std::thread::scope(move |s| {
+                let term_copy = term.clone();
+                let hvm_handle = s.spawn(move || {
+                    runtime.normalize_term(&term)
+                });
+                let interp_handle = s.spawn(move || {
+                    interpreter::complete_dups(interpreter::normalize(term_copy))
+                });
+                let sec = std::time::Duration::from_secs(1);
+                for _ in 0..60 {
+                    if !interp_handle.is_finished() {
+                        std::thread::sleep(sec);
+                    } else if !hvm_handle.is_finished() {
+                        // HVM should have terminated by now
+                        return None;
+                    } else {
+                        return Some((hvm_handle.join(), interp_handle.join()));
+                    }
+                }
+                panic!("term (possibly) does not terminate");
+            });
+        });
+
         // TODO: asset!(isomorphic(expected_result, result));
         // variable names might be different
     })
